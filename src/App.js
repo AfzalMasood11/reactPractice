0948e13133e87b0cc810bba6064c050f7b66699e
Afzal Masood <afzal.masood11@gmail.com>
2023-09-22 19:18:54 +0500

    Add first component.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import FirstComponent from './components/FirstComponent';
 
 function App() {
   return (
@@ -18,6 +19,7 @@ function App() {
           Learn React
         </a>
       </header>
+      <FirstComponent/>
     </div>
   );
 }
---
+++ src/components/FirstComponent.js
@@ -0,0 +1,5 @@
+export default function() {
+    return(
+        <p>It is my first component!</p>
+    );
+}
