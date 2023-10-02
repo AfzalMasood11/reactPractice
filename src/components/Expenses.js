006a39f078dfefee340774a655e4b3ca44598e46
Afzal Masood <afzal.masood11@gmail.com>
2023-10-02 16:20:25 +0500

    Remove the duplication of expenseItem component.

--- src/components/Expenses.js
+++ src/components/Expenses.js
@@ -2,28 +2,16 @@ import ExpenseItem from "./ExpenseItem";
 import './Expenses.css'
 
 function Expenses(props){
+    
     return(
         <div className="expenses">
-            <ExpenseItem
-                title={props.items[0].title}
-                amount={props.items[0].amount}
-                date={props.items[0].date}
-            />
-            <ExpenseItem
-                title={props.items[1].title}
-                amount={props.items[1].amount}
-                date={props.items[1].date}
-            />
-            <ExpenseItem
-                title={props.items[2].title}
-                amount={props.items[2].amount}
-                date={props.items[2].date}
-            />
-            <ExpenseItem
-                title={props.items[3].title}
-                amount={props.items[3].amount}
-                date={props.items[3].date}
-            />
+            {props.items.map((item) => (
+                <ExpenseItem
+                    title={item.title}
+                    amount={item.amount}
+                    date={item.date}
+                />  
+            ))}        
         </div>
     );
 }
